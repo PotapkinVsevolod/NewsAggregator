2e1c2c4b9e3cf2efc116ce2470f01f7b2139981b
potapkinvsevolod <potapkinvsevolod@gmail.com>
2021-02-05 22:08:31 +0700

    Сократил получение списка последних статей из БД до одного запроса. Внес функцию парсера в handle()

--- news_aggregator/aggregator/management/commands/parse_news.py
+++ news_aggregator/aggregator/management/commands/parse_news.py
@@ -11,31 +11,22 @@ from aggregator.models import News
 PAUSE = 60*60
 
 
-def _parse_news(last_news_titles):
-    while True:
-        response = requests.get('https://news.ycombinator.com')
-        soup = bs4.BeautifulSoup(response.text, 'lxml')
-        news = soup.findAll('tr', {'class': 'athing'})
-        for item in reversed(news):
-            title = item.find('a', 'storylink').getText()
-            link = item.find('a', 'storylink')['href']
-            if title not in last_news_titles:
-                News.objects.create(title=title, link=link).save()
-        print(f'News downloads in {timezone.now()}.')
-        time.sleep(PAUSE)
-
-
-def _get_last_news_titles_from_db():
-    last_news = News.objects.order_by('-id')[:31]
-    last_news_titles = []
-    for item in last_news:
-        last_news_titles.append(item.title)
-    return last_news_titles
-
-
 class Command(BaseCommand):
     help = 'Start periodical parsing of Hacker News and save result to db.'
 
     def handle(self, *args, **options):
-        last_news_titles = _get_last_news_titles_from_db()
-        _parse_news(last_news_titles=last_news_titles)
+        while True:
+            last_news_titles = News.objects.order_by('-id')[:30].values_list('title', flat=True)
+            response = requests.get('https://news.ycombinator.com')
+            soup = bs4.BeautifulSoup(response.text, 'lxml')
+            news = soup.findAll('tr', {'class': 'athing'})
+
+            for item in reversed(news):
+                title = item.find('a', 'storylink').getText()
+                link = item.find('a', 'storylink')['href']
+
+                if title not in last_news_titles:
+                    News.objects.create(title=title, link=link)
+
+            print(f'News downloads in {timezone.now()}.')
+            time.sleep(PAUSE)
