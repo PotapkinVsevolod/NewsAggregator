f54781b65c6b85248cbad6e3237a951049b13b6c
potapkinvsevolod <potapkinvsevolod@gmail.com>
2021-02-07 14:16:29 +0700

    Объединил запросы через bulk_create.

--- news_aggregator/aggregator/management/commands/parse_news.py
+++ news_aggregator/aggregator/management/commands/parse_news.py
@@ -17,6 +17,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
             last_news_titles = News.objects.order_by('-id')[:30].values_list('title', flat=True)
+
             response = requests.get('https://news.ycombinator.com')
             soup = bs4.BeautifulSoup(response.text, 'lxml')
             news = soup.findAll('tr', {'class': 'athing'})
@@ -25,8 +26,10 @@ class Command(BaseCommand):
                 title = item.find('a', 'storylink').getText()
                 link = item.find('a', 'storylink')['href']
 
+                bulk_creates = []
                 if title not in last_news_titles:
-                    News.objects.create(title=title, link=link)
+                    bulk_creates.append(News(title=title, link=link))
+                News.objects.bulk_create(bulk_creates)
 
             print(f'News downloads in {timezone.now()}.')
             time.sleep(PAUSE)
