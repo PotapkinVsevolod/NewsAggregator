fbb1fa4e723a18cd72ff2115da6138be1fb88ff9
potapkinvsevolod <potapkinvsevolod@gmail.com>
2021-02-08 11:00:17 +0700

    Сократил время запроса, забирая из БД только заголовки, а только потом взяв слайс оттуда. Добавил проверку на наличие bulk_create запросов. Добавил возможность из командной строки добавлять занести паузу и туда же передал значение по умолчанию.

--- news_aggregator/aggregator/management/commands/parse_news.py
+++ news_aggregator/aggregator/management/commands/parse_news.py
@@ -8,16 +8,12 @@ from django.utils import timezone
 from aggregator.models import News
 
 
-PAUSE = 60*60
-
-
 class Command(BaseCommand):
     help = 'Start periodical parsing of Hacker News and save result to db.'
 
     def handle(self, *args, **options):
         while True:
-            last_news_titles = News.objects.order_by('-id')[:30].values_list('title', flat=True)
-
+            last_news_titles = News.objects.order_by('-id').values_list('title', flat=True)[:30]
             response = requests.get('https://news.ycombinator.com')
             soup = bs4.BeautifulSoup(response.text, 'lxml')
             news = soup.findAll('tr', {'class': 'athing'})
@@ -29,7 +25,19 @@ class Command(BaseCommand):
                 bulk_creates = []
                 if title not in last_news_titles:
                     bulk_creates.append(News(title=title, link=link))
-                News.objects.bulk_create(bulk_creates)
 
+                if bulk_creates:
+                    News.objects.bulk_create(bulk_creates)
+
+            options['pause'] = int(options['pause'])
             print(f'News downloads in {timezone.now()}.')
-            time.sleep(PAUSE)
+            time.sleep(options['pause'])
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-p',
+            '--pause',
+            action='store',
+            default=60*60,
+            help='Time between parsing in seconds'
+        )
